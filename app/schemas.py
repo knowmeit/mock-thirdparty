bdb59b4b0473b4daa63de563c8cb6d1220d503cc
faraz <faraz.fatahnaie@gmail.com>
2024-11-02 13:38:12 +0330

    refactor

--- app/schemas.py
+++ app/schemas.py
@@ -13,19 +13,3 @@ class TakeResultResponse(BaseModel):
     similarity: Optional[float] = None
     verification_flag: Optional[bool] = Field(None, alias="verification-flag")
     liveness_flag: Optional[bool] = Field(None, alias="liveness-flag")
-
-# Example usage
-example_data = {
-    "national_code": "۰۰۲۱۲۱۹۹۵۸",
-    "first_name": "سیدفراز",
-    "last_name": "فتحنائی اصل",
-    "birthdate": "۱۳۷۷-۰۴-۲۴",
-    "father_name": "سیدفرشید",
-    "expire_date": "۱۴۰۱-۰۴۳۱",
-    "similarity": 0.4414507,
-    "verification-flag": True,
-    "liveness-flag": False
-}
-
-response = TakeResultResponse(**example_data)
-print(response)
